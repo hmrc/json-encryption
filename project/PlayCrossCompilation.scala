c815669f97c5c2ffda6c8cb07dc02aff02f62bc2
Christopher Turner <23338096+christopherjturner@users.noreply.github.com>
2020-06-10 10:34:11 +0100

    BDOG-871 update cross compile settings

--- project/PlayCrossCompilation.scala
+++ project/PlayCrossCompilation.scala
@@ -1,11 +1,5 @@
-import uk.gov.hmrc.playcrosscompilation.{AbstractPlayCrossCompilation, PlayVersion}
+import uk.gov.hmrc.playcrosscompilation.AbstractPlayCrossCompilation
 import uk.gov.hmrc.playcrosscompilation.PlayVersion.Play25
 
-object PlayCrossCompilation extends AbstractPlayCrossCompilation(defaultPlayVersion = Play25){
-  override def playCrossScalaBuilds(scalaVersions: Seq[String]): Seq[String] =
-    playVersion match {
-      case PlayVersion.Play25 => scalaVersions.filter(version => version.startsWith("2.11"))
-      case PlayVersion.Play26 => scalaVersions
-      case PlayVersion.Play27 => scalaVersions.filter(version => version.startsWith("2.12"))
-    }
-}
+object PlayCrossCompilation extends AbstractPlayCrossCompilation(defaultPlayVersion = Play25)
+
